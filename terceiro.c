ddc49e8bb819afdd31f611989a6bf6f6587d78ad
Josicler Leme Silva <josiclerdeveloper@gmail.com>
2021-07-17 15:02:22 -0400

    Foi criado mais um arquivo com sucesso!

---
+++ terceiro.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <locale.h>
+/*
+    Programa criado por Josicler Leme Silva
+*/
+void main() {
+    setlocale(LC_ALL, "Portuguese");
+    printf("C é \n\"SUPER\" \nfácil!");
+}
